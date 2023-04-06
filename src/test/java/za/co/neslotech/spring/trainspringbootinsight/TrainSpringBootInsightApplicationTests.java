d46eb4c9b2674b98eb87ea8c08ee55805d9d1417
r-barlow <rbarlow@neslotech.co.za>
2023-04-06 10:06:58 +0200

    TRAIN-213: App startup and config.

---
+++ src/test/java/za/co/neslotech/spring/trainspringbootinsight/TrainSpringBootInsightApplicationTests.java
@@ -0,0 +1,13 @@
+package za.co.neslotech.spring.trainspringbootinsight;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainSpringBootInsightApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
