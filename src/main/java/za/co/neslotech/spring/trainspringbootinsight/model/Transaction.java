c93965ba0c68e37fa3964aff3a81650db52f4787
r-barlow <rbarlow@neslotech.co.za>
2023-04-11 11:09:42 +0200

    TRAIN-210: Create Transaction Model

---
+++ src/main/java/za/co/neslotech/spring/trainspringbootinsight/model/Transaction.java
@@ -0,0 +1,55 @@
+package za.co.neslotech.spring.trainspringbootinsight.model;
+
+import jakarta.persistence.*;
+
+import java.sql.Date;
+
+@Entity(name = "transactions")
+public class Transaction {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "transaction_id")
+    private Long transactionId;
+
+    @Column(name = "transaction_date")
+    private Date transactionDate;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "amount", length = 19, precision = 4)
+    private Double amount;
+
+    public Long getTransactionId() {
+        return transactionId;
+    }
+
+    public void setTransactionId(Long transactionId) {
+        this.transactionId = transactionId;
+    }
+
+    public Date getTransactionDate() {
+        return transactionDate;
+    }
+
+    public void setTransactionDate(Date transactionDate) {
+        this.transactionDate = transactionDate;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Double amount) {
+        this.amount = amount;
+    }
+}
