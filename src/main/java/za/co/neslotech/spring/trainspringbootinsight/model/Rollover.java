c6d0feaa5ac07b502d119b77a23b578d2ba907d4
r-barlow <rbarlow@neslotech.co.za>
2023-04-11 10:20:44 +0200

    TRAIN-208: Create Category Model and made changes to Rollover model

--- src/main/java/za/co/neslotech/spring/trainspringbootinsight/model/Rollover.java
+++ src/main/java/za/co/neslotech/spring/trainspringbootinsight/model/Rollover.java
@@ -8,8 +8,24 @@ public class Rollover {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "rollover_id")
-    private Long RolloverId;
+    private Long rolloverId;
 
     @Column(name = "name")
     private String name;
+
+    public Long getRolloverId() {
+        return rolloverId;
+    }
+
+    public void setRolloverId(Long rolloverId) {
+        this.rolloverId = rolloverId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
