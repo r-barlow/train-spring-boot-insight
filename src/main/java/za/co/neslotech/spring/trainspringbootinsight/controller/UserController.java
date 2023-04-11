7e91c91d1b629b7086f326ce6003192f1515bde9
r-barlow <rbarlow@neslotech.co.za>
2023-04-11 15:10:27 +0200

    TRAIN-216: Migrate user controller to generic.

--- src/main/java/za/co/neslotech/spring/trainspringbootinsight/controller/UserController.java
+++ src/main/java/za/co/neslotech/spring/trainspringbootinsight/controller/UserController.java
@@ -1,60 +1,30 @@
 package za.co.neslotech.spring.trainspringbootinsight.controller;
 
-import jakarta.persistence.EntityNotFoundException;
-import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.web.bind.annotation.*;
 import za.co.neslotech.spring.trainspringbootinsight.model.User;
 import za.co.neslotech.spring.trainspringbootinsight.repository.IUserRepository;
 
-import java.util.List;
-
 @RestController
 @RequestMapping("/api/v1/users")
-public class UserController {
+public class UserController extends AGenericController<User, Long>{
 
     @Autowired
     private IUserRepository userRepository;
 
-    @GetMapping
-    public List<User> list(){
-        return userRepository.findAll();
+    @Override
+    protected JpaRepository<User, Long> getRepository() {
+        return userRepository;
     }
 
-    @GetMapping
-    @RequestMapping("{id}")
-    public User get(@PathVariable Long id){
-        return userRepository.findById(id).orElseThrow(() ->
-                new EntityNotFoundException(String.format("The User with id %d was not found!", id))
-        );
+    @Override
+    protected String getEntityNotFoundDescription(Long id) {
+        return String.format("The User with id %d was not found!", id);
     }
 
-    @PostMapping
-    @ResponseStatus(HttpStatus.CREATED)
-    public User create(@RequestBody User user){
-        return userRepository.saveAndFlush(user);
+    @Override
+    protected String[] getUpdateColumnExclusions() {
+        return new String[]{"userId"};
     }
-
-    @RequestMapping(value = "{id}", method = RequestMethod.DELETE)
-    public void delete(@PathVariable Long id) {
-        User user = userRepository.findById(id).orElseThrow(() ->
-                new EntityNotFoundException(String.format("The User with id %d was not found!", id))
-        );
-
-        userRepository.delete(user);
-    }
-
-    @RequestMapping(value = "{id}", method = RequestMethod.PUT)
-    public User update(@PathVariable Long id, @RequestBody User user){
-
-        User existingUser = userRepository.findById(id).orElseThrow(() ->
-                new EntityNotFoundException(String.format("The User with id %d was not found!", id))
-        );
-
-        BeanUtils.copyProperties(user, existingUser, "userId");
-
-        return userRepository.saveAndFlush(existingUser);
-    }
-
 }
