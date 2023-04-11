866a80cd2aa54f35b0351647d29f804ced87660d
r-barlow <rbarlow@neslotech.co.za>
2023-04-11 10:42:32 +0200

    TRAIN-209: Create Account Model

---
+++ src/main/java/za/co/neslotech/spring/trainspringbootinsight/model/Account.java
@@ -0,0 +1,31 @@
+package za.co.neslotech.spring.trainspringbootinsight.model;
+
+import jakarta.persistence.*;
+
+@Entity(name = "account")
+public class Account {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "account_id")
+    private Long accountId;
+
+    @Column(name = "name")
+    private String name;
+
+    public Long getAccountId() {
+        return accountId;
+    }
+
+    public void setAccountId(Long accountId) {
+        this.accountId = accountId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
