730681c778ffba11be586d306d94c868f7870a07
r-barlow <rbarlow@neslotech.co.za>
2023-04-12 14:11:32 +0200

    TRAIN-208: Fix imports and make props final

--- src/main/java/za/co/neslotech/spring/trainspringbootinsight/model/Category.java
+++ src/main/java/za/co/neslotech/spring/trainspringbootinsight/model/Category.java
@@ -1,36 +1,40 @@
 package za.co.neslotech.spring.trainspringbootinsight.model;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
 @Entity(name = "categories")
 public class Category {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long categoryId;
+    private Long id;
 
-    @Column(name = "name")
+    @Column
     private String name;
 
-    @Column(name = "description")
+    @Column
     private String description;
 
-    @Column(name = "expected", length = 19, precision = 4)
+    @Column(length = 19, precision = 4)
     private Double expected;
 
-    public Long getCategoryId() {
-        return categoryId;
+    public Long getId() {
+        return id;
     }
 
-    public void setCategoryId(Long categoryId) {
-        this.categoryId = categoryId;
+    public void setId(final Long id) {
+        this.id = id;
     }
 
     public String getName() {
         return name;
     }
 
-    public void setName(String name) {
+    public void setName(final String name) {
         this.name = name;
     }
 
@@ -38,7 +42,7 @@ public class Category {
         return description;
     }
 
-    public void setDescription(String description) {
+    public void setDescription(final String description) {
         this.description = description;
     }
 
@@ -46,7 +50,7 @@ public class Category {
         return expected;
     }
 
-    public void setExpected(Double expected) {
+    public void setExpected(final Double expected) {
         this.expected = expected;
     }
 }
