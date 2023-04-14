a6897828e20bdbab77f18c3cc10a795c4c37b51a
r-barlow <rbarlow@neslotech.co.za>
2023-04-14 10:44:59 +0200

    TRAIN-217: Implement generic Rollover controller.

--- src/main/java/za/co/neslotech/spring/trainspringbootinsight/controller/RolloverController.java
+++ src/main/java/za/co/neslotech/spring/trainspringbootinsight/controller/RolloverController.java
@@ -1,31 +1,25 @@
 package za.co.neslotech.spring.trainspringbootinsight.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import za.co.neslotech.spring.trainspringbootinsight.model.Rollover;
-import za.co.neslotech.spring.trainspringbootinsight.repository.IRolloverRepository;
+import za.co.neslotech.spring.trainspringbootinsight.service.CrudService;
+import za.co.neslotech.spring.trainspringbootinsight.service.RolloverService;
 
 @RestController
 @RequestMapping("/api/v1/rollovers")
-public class RolloverController extends AGenericController<Rollover, Long> {
+public class RolloverController extends CrudController<Rollover, Long> {
+
+    private final RolloverService service;
 
     @Autowired
-    private IRolloverRepository rolloverRepository;
-
-    @Override
-    protected JpaRepository<Rollover, Long> getRepository() {
-        return rolloverRepository;
+    public RolloverController(final RolloverService service) {
+        this.service = service;
     }
 
     @Override
-    protected String getEntityNotFoundDescription(final Long id) {
-        return String.format("The Rollover with id %d was not found!", id);
-    }
-
-    @Override
-    protected String[] getUpdateColumnExclusions() {
-        return new String[]{"id"};
+    protected CrudService<Rollover, Long> getService() {
+        return service;
     }
 }
