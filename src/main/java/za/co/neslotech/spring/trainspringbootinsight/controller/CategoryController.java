e85505e6f197b1099d64660ed8ed035c3ad86b28
r-barlow <rbarlow@neslotech.co.za>
2023-04-17 09:12:25 +0200

    TRAIN-220: Implement generic Account controller.

--- src/main/java/za/co/neslotech/spring/trainspringbootinsight/controller/CategoryController.java
+++ src/main/java/za/co/neslotech/spring/trainspringbootinsight/controller/CategoryController.java
@@ -1,31 +1,23 @@
 package za.co.neslotech.spring.trainspringbootinsight.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import za.co.neslotech.spring.trainspringbootinsight.model.Category;
-import za.co.neslotech.spring.trainspringbootinsight.repository.ICategoryRepository;
+import za.co.neslotech.spring.trainspringbootinsight.service.CategoryService;
+import za.co.neslotech.spring.trainspringbootinsight.service.CrudService;
 
 @RestController
 @RequestMapping("/api/v1/categories")
-public class CategoryController extends AGenericController<Category, Long> {
+public class CategoryController extends CrudController<Category, Long> {
 
-    @Autowired
-    private ICategoryRepository categoryRepository;
+    private final CategoryService service;
 
-    @Override
-    protected JpaRepository<Category, Long> getRepository() {
-        return categoryRepository;
+    public CategoryController(final CategoryService service) {
+        this.service = service;
     }
 
     @Override
-    protected String getEntityNotFoundDescription(Long id) {
-        return String.format("The Category with id %d was not found!", id);
-    }
-
-    @Override
-    protected String[] getUpdateColumnExclusions() {
-        return new String[]{"categoryId"};
+    protected CrudService<Category, Long> getService() {
+        return service;
     }
 }
---
+++ src/main/java/za/co/neslotech/spring/trainspringbootinsight/service/CategoryService.java
@@ -0,0 +1,56 @@
+package za.co.neslotech.spring.trainspringbootinsight.service;
+
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.stereotype.Service;
+import za.co.neslotech.spring.trainspringbootinsight.model.Category;
+import za.co.neslotech.spring.trainspringbootinsight.repository.ICategoryRepository;
+
+import java.util.List;
+
+@Service
+public class CategoryService implements CrudService<Category, Long> {
+
+    private final ICategoryRepository repository;
+
+    public CategoryService(final ICategoryRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public Category create(final Category entity) {
+        return repository.saveAndFlush(entity);
+    }
+
+    @Override
+    public Category update(final Category entity) {
+        return repository.saveAndFlush(entity);
+    }
+
+    @Override
+    public List<Category> findAll() {
+        return repository.findAll();
+    }
+
+    @Override
+    public Category findById(final Long id) {
+        return repository.findById(id)
+                .orElseThrow(() ->
+                        new EntityNotFoundException(String.format("The Category entity with id %d was not found!", id))
+                );
+    }
+
+    @Override
+    public void delete(final Category entity) {
+        repository.delete(entity);
+    }
+
+    @Override
+    public Long getEntityId(final Category entity) {
+        return entity.getId();
+    }
+
+    @Override
+    public String[] getUpdateColumnExclusions() {
+        return new String[]{"id"};
+    }
+}
