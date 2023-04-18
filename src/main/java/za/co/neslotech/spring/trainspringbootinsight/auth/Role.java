7fc046c19c9523cc0cba5992d99d89da3df0c4cf
r-barlow <rbarlow@neslotech.co.za>
2023-04-18 11:08:00 +0200

    TRAIN-242: Update user model.

---
+++ src/main/java/za/co/neslotech/spring/trainspringbootinsight/auth/Role.java
@@ -0,0 +1,6 @@
+package za.co.neslotech.spring.trainspringbootinsight.auth;
+
+public enum Role {
+    USER,
+    ADMIN
+}
--- src/main/java/za/co/neslotech/spring/trainspringbootinsight/model/User.java
+++ src/main/java/za/co/neslotech/spring/trainspringbootinsight/model/User.java
@@ -1,19 +1,24 @@
 package za.co.neslotech.spring.trainspringbootinsight.model;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import za.co.neslotech.spring.trainspringbootinsight.auth.Role;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 @Entity(name = "users")
-public class User {
+public class User implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -29,6 +34,13 @@ public class User {
     @Column(name = "last_name")
     private String lastName;
 
+    @Column
+    private String password;
+
+    @Column
+    @Enumerated
+    private Role role;
+
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
     private List<Budget> budgets;
 
@@ -38,14 +50,6 @@ public class User {
 
     public void setId(final Long userId) {
         this.id = userId;
-    }
-
-    public String getUserName() {
-        return username;
-    }
-
-    public void setUserName(final String userName) {
-        this.username = userName;
     }
 
     public String getFirstName() {
@@ -64,12 +68,63 @@ public class User {
         this.lastName = lastName;
     }
 
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(final Role role) {
+        this.role = role;
+    }
+
     public List<Budget> getBudgets() {
         return budgets == null ? new ArrayList<>() : budgets;
     }
 
     public void setBudgets(final List<Budget> budgets) {
         this.budgets = budgets;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.name()));
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(final String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(final String username) {
+        this.username = username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
     }
 }
 
